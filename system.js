2c78cbd9b4a5b29629a602c71e498805cbfb4523
kanaria-LC <itsuki.tani2@gmail.com>
2025-01-22 21:01:56 +0900

    Update system.js

--- system.js
+++ system.js
@@ -22,54 +22,67 @@ const leisatescript = {
     },
     gameleisate:{
         canvas:{
-            create:function(creation_name,creation_width,creation_height,type){
+            create:function(creation_name,creation_width,creation_height,type,iszindex){
                 const canvas = document.createElement('canvas');
+                canvas.style.zIndex = iszindex
+                canvas.style.position = 'absolute'
                 canvas.id = creation_name;
                 canvas.width = creation_width;
                 canvas.height = creation_height;
                 leisatescript.date.creation[creation_name].htmlcode ={ element: canvas, type: type };
-                document.body.appendChild(canvas);},
+                document.getElementById('stage').appendChild(canvas);},
             adaptation:function(creation_name,typemode,cx,cy,creation_width,creation_height){
-                if(typemode[0] == '#' && leisatescript.date.creation[creation_name].htmlcode.type == 'colorfill'){
-                    const cre = document.getElementById(creation_name);
-                    const ctx = cre.getContext('2d');
+                const cre = document.getElementById(creation_name);
+                const ctx = cre.getContext('2d');
+                ctx.clearRect(0, 0, cre.width, cre.height)
+                if(leisatescript.date.creation[creation_name].htmlcode.type == 'colorfill'){
                     ctx.fillStyle = typemode;
                     ctx.fillRect(cx, cy,creation_width,creation_height);
-                    document.getElementById('stage').appendChild(cre);   
+                }
+                if(leisatescript.date.creation[creation_name].htmlcode.type == 'image'){
+                    const img = new Image();
+                    img.onload = function(){
+                        ctx.drawImage(img, cx, cy, creation_width, creation_height)
+                    }
+                    img.src = typemode
                 }
             }
         },
     }
 };
 leisatescript.date.creation = {};
-const a = leisatescript.creation.alteration;
 const c = leisatescript.creation.create;
 const cv = leisatescript.gameleisate.canvas;
 c.creation('test')
-cv.create('test',800,600,'colorfill');
-function move() {
-    const cre = document.getElementById('test');
-    const ctx = cre.getContext('2d');
-    ctx.clearRect(0, 0, cre.width, cre.height)
-    cv.adaptation('test','#00ffff',isx,isy,100,100)
-}
+c.creation('test2')
+cv.create('test2',1310,744,'colorfill',1);
+cv.create('test',1310,744,'colorfill',2);
 let isx = 0;
 let isy = 0;
+const isFPS = {}
+function game_roop() {
+    const speed = 5;
+    if (isFPS['ArrowUp'] && isy >= 0) {
+        isy -= speed;
+    }
+    if (isFPS['ArrowRight'] && isx <= 1210) {
+        isx += speed;
+    }
+    if (isFPS['ArrowDown'] && isy <= 644) {
+        isy += speed;
+    }
+    if (isFPS['ArrowLeft'] && isx >= 0) {
+        isx -= speed;
+    }
+    cv.adaptation('test2','#00ff00',0,0,500,500)
+    cv.adaptation('test','#0000ff',isx,isy,500,340)
+    requestAnimationFrame(game_roop)
+}
 window.addEventListener("keydown",function(event){
-    const key = event.key
-        console.log(isy,isx,leisatescript)
-        if (key === 'ArrowUp') {
-            isy -= 10;
-        }
-        if (key === 'ArrowRight') {
-            isx += 10;
-        }
-        if (key === 'ArrowDown') {
-            isy += 10;
-        }
-        if (key === 'ArrowLeft') {
-            isx -= 10;
-        }
-    move();
+    isFPS[event.key] = true;
+})
+window.addEventListener("keyup",function(event){
+    isFPS[event.key] = false;
 })
 
+game_roop();
