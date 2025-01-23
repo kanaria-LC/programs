a392c73536c5fff66938f8f246273b476f8da774
kanaria-LC <itsuki.tani2@gmail.com>
2025-01-23 20:56:52 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+leisatescript.date.creation = {};
+const c = leisatescript.creation.create;
+const cv = leisatescript.gameleisate.canvas;
+c.creation('test')
+c.creation('test2')
+cv.create('test2',1310,744,'colorfill',1);
+cv.create('test',1310,744,'colorfill',2);
+
+const isFPS = {}
+const ber ={
+    y:0,
+    move:function () {
+        const speed = 5;
+        if (isFPS['ArrowUp'] && ber.y >= 0) {
+            ber.y -= speed;
+        }
+        if (isFPS['ArrowDown'] && ber.y <= 644) {
+            ber.y += speed;
+        }
+        cv.adaptation('test','#0000ff',0,ber.y,25,100)
+        requestAnimationFrame(ber.move)
+    },
+
+}
+let hp = 100;
+const atackarrow = {
+    x: 1310,
+    y: 0,
+    aru: true,
+    move: function () {
+        const speed = 5;
+        if (atackarrow.aru) {
+            
+            atackarrow.x -= speed;
+            if (atackarrow.x <= 25 && atackarrow.y >= ber.y && atackarrow.y <= (ber.y + 100)) {
+                atackarrow.aru = false; 
+            }
+            if (atackarrow.x <= -50) {
+                atackarrow.aru = false; 
+                hp -= 10
+            }
+            cv.adaptation('test2', '#0000ff', atackarrow.x, atackarrow.y, 25, 25);
+            if((atackarrow.x <= 25 && atackarrow.y >= ber.y && atackarrow.y <= (ber.y + 100))||atackarrow.x <= -50){
+                cv.adaptation('test2', '#0000ff',0,0,0,0);
+            }
+        }
+        if (atackarrow.aru) {
+            requestAnimationFrame(atackarrow.move);
+        }
+        if(!atackarrow.aru){
+            atackarrow.y = getRandomInt(0,744);
+            atackarrow.x = 1310;
+            atackarrow.aru = true
+            requestAnimationFrame(atackarrow.move);
+        }
+    }
+};
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+window.addEventListener("keydown",function(event){
+    isFPS[event.key] = true;
+})
+window.addEventListener("keyup",function(event){
+    isFPS[event.key] = false;
+})
+atackarrow.y = getRandomInt(0,744)
+ber.move();
+atackarrow.move()
+
